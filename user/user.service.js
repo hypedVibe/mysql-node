878196f101b2d2160a08e1b5e816aa32687ecfa7
Kate Komar <komarkateryna@gmail.com>
2018-09-26 14:48:00 +0300

    add checking the user existense

--- user/user.service.js
+++ user/user.service.js
@@ -1,18 +1,23 @@
 const User = require('../models/index').User;
 
+const ResponseError = require('../errors/reponseError');
+
 exports.createProfile = async (userData) => {
   const profile = await User.create(userData);
   return profile;
 };
 
 exports.updateProfile = async (userId, userData) => {
-  await User.update(userData, { where: { id: userId } });
   const profile = await exports.getProfile(userId);
+  await User.update(userData, { where: { id: userId } });
   return profile;
 };
 
 exports.getProfile = async (userId) => {
   const profile = await User.findOne({ where: { id: userId } });
+  if (!profile) {
+    throw new ResponseError(`User with ${userId} id wasn't found`, 404);
+  }
   return profile;
 };
 
