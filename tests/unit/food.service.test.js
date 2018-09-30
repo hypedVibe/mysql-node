2c1a3e97fe877d564e2f236bdb9fbf88e8e55121
hypedVibe <komarkateryna@gmail.com>
2018-09-30 22:26:46 +0300

    add mocks for food service unit testing

---
+++ tests/unit/food.service.test.js
@@ -0,0 +1,14 @@
+// /* global describe, it */
+
+// const chai = require('chai');
+// const expect = chai.expect;
+
+// chai.use(require('sinon-chai'));
+// chai.use(require('chai-as-promised'));
+
+// const userService = require('../../features/food/food.service');
+
+// const ResponseError = require('../../errors/reponseError');
+
+// const Food = require('../modelsMock/Food');
+// const UserService = require('../servicesMock/user.service');
